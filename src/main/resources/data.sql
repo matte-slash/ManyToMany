5f6b28209b0e3116c7833d4bffa9d57ba7fd232b
matte-slash <matteorosso92@gmail.com>
2022-11-10 11:33:22 +0100

    add Postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,17 @@
-MERGE INTO Company
-    (id,c_name,capital)
-    KEY (id)
-VALUES (1,'Apple',9999),
-    (2,'Google',9999);
+INSERT INTO Company (c_name,capital)
+    select t.*
+    from ((SELECT  'Apple' as c_name, 9999 as capital
+          ) union all
+          (SELECT 'Google', 9999
+          )
+         ) t
+WHERE NOT EXISTS (
+    SELECT * FROM Company
+);
 
 
-MERGE INTO Author
-    (id,first_name,last_name)
-    KEY(id)
-VALUES (1,'Matteo','Rosso');
+INSERT INTO Author (first_name,last_name)
+    SELECT 'Matteo','Rosso'
+WHERE NOT EXISTS (
+    SELECT * FROM Author
+);
